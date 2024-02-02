41a23fdaf73ca8c5c3c9dfe5275df7f814ef7a71
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-01 21:26:47 -0500

    started frontend, piano created

---
+++ front-end/src/Components/Piano.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Piano.css";
+
+const Piano: React.FC = () => {
+  return (
+    <div>
+      <div className="piano">
+        <div className="r2"></div>
+        <div className="r3"></div>
+        <div className="r4"></div>
+        <div className="r5"></div>
+        <div className="r6"></div>
+        <div className="r7"></div>
+        <div className="r8"></div>
+        <div className="r9"></div>
+        <div className="r10"></div>
+        <div className="r11"></div>
+        <div className="r12"></div>
+        <div className="r13"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Piano;
