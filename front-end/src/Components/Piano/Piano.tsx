67a1692bcb4b941691b34bcd5a13a324241221a6
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-01 22:33:47 -0500

    changed piano to buttons

---
+++ front-end/src/Components/Piano/Piano.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Piano.css";
+
+const Piano: React.FC = () => {
+  return (
+    <div>
+      <div className="piano">
+        <button className="r2"></button>
+        <button className="r3"></button>
+        <button className="r4"></button>
+        <button className="r5"></button>
+        <button className="r6"></button>
+        <button className="r7"></button>
+        <button className="r8"></button>
+        <button className="r9"></button>
+        <button className="r10"></button>
+        <button className="r11"></button>
+        <button className="r12"></button>
+        <button className="r13"></button>
+      </div>
+    </div>
+  );
+};
+
+export default Piano;
