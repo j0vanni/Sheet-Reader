cc70b7ab3bf77a11d458a71ad5a16e25042760de
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-03 18:04:54 -0500

    - Add PianoTypes - Change the way the piano gets rendered - Update note/key naming

--- front-end/src/ts/piano/Piano.tsx
+++ front-end/src/ts/piano/Piano.tsx
@@ -1,23 +1,47 @@
 import React from "react";
 import "./Piano.css";
+import { PianoKey, Note } from "./PianoTypes";
+
+const keys: PianoKey[] = [
+  { note: Note.C4, whiteKey: true },
+  { note: Note.CD4, blackKey: true },
+  { note: Note.D4, whiteKey: true },
+  { note: Note.DE4, blackKey: true },
+  { note: Note.E4, whiteKey: true },
+  { note: Note.F4, whiteKey: true },
+  { note: Note.FG4, blackKey: true },
+  { note: Note.G4, whiteKey: true },
+  { note: Note.GA4, blackKey: true },
+  { note: Note.A4, whiteKey: true },
+  { note: Note.AB4, blackKey: true },
+  { note: Note.B4, whiteKey: true },
+];
 
 const Piano: React.FC = () => {
+  const whiteKeys = keys.map((key, index) =>
+    key.whiteKey ? (
+      <button
+        key={index}
+        className="white-key"
+        onClick={() => console.log(key.note)}
+      ></button>
+    ) : null
+  );
+
+  const blackKeys = keys.map((key, index) =>
+    key.blackKey ? (
+      <button
+        key={index}
+        className="black-key"
+        onClick={() => console.log(key.note)}
+      ></button>
+    ) : null
+  );
+
   return (
-    <div>
-      <div className="piano">
-        <button className="c-note"></button>
-        <button className="cd-note"></button>
-        <button className="d-note"></button>
-        <button className="de-note"></button>
-        <button className="e-note"></button>
-        <button className="f-note"></button>
-        <button className="fg-note"></button>
-        <button className="g-note"></button>
-        <button className="ga-note"></button>
-        <button className="a-note"></button>
-        <button className="ab-note"></button>
-        <button className="b-note"></button>
-      </div>
+    <div className="piano-container">
+      <div className="white-keys-container">{whiteKeys}</div>
+      <div className="black-keys-container">{blackKeys}</div>
     </div>
   );
 };
---
+++ front-end/src/ts/piano/PianoTypes.tsx
@@ -0,0 +1,24 @@
+export enum Note {
+  C4 = "C4",
+  D4 = "D4",
+  E4 = "E4",
+  F4 = "F4",
+  G4 = "G4",
+  A4 = "A4",
+  B4 = "B4",
+  CD4 = "CD4",
+  DE4 = "DE4",
+  FG4 = "FG4",
+  GA4 = "GA4",
+  AB4 = "AB4",
+}
+
+export interface PianoKey {
+  note: Note;
+  whiteKey?: boolean;
+  blackKey?: boolean;
+}
+
+export interface PianoProps {
+  keys: PianoKey[];
+}
