20943f88599c7805d5e9390f17f1932425493ebe
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-03 18:02:20 -0500

    Refactor PianoTypes namespace to SheetTypes

--- front-end/src/ts/types/types.tsx
+++ front-end/src/ts/types/types.tsx
@@ -1,30 +1,4 @@
-declare namespace PianoTypes {
-  enum Note {
-    C = "C",
-    D = "D",
-    E = "E",
-    F = "F",
-    G = "G",
-    A = "A",
-    B = "B",
-    CSharp = "C#",
-    DSharp = "D#",
-    FSharp = "F#",
-    GSharp = "G#",
-    ASharp = "A#",
-    DFlat = "Db",
-    EFlat = "Eb",
-    GFlat = "Gb",
-    AFlat = "Ab",
-    BFlat = "Bb",
-  }
-
-  interface PianoKey {
-    note: Note;
-    isFlat?: boolean;
-    isSharp?: boolean;
-  }
-
+declare namespace SheetTypes {
   type Clef = "treble" | "bass" | "both";
 
   type Duration = "15" | "30" | "60";
@@ -37,5 +11,6 @@ declare namespace PianoTypes {
     clef: Clef;
     duration: Duration;
     timeSignature: TimeSignature;
+    userStart?: boolean;
   }
 }
