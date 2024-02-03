0345bfa905546b877db0065fff8f2042d98d559a
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-03 16:19:01 -0500

    - Changed folder names - Added types.tsx

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import Piano from "./Components/Piano/Piano.tsx";
+import Piano from "./ts/piano/Piano.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
