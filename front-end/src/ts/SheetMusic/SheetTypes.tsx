76f83ad29917aa61658ebe5a6d3c7daf8dbc563d
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-04 14:00:29 -0500

    Remove unused Duration type from SheetTypes.tsx

--- front-end/src/ts/SheetMusic/SheetTypes.tsx
+++ front-end/src/ts/SheetMusic/SheetTypes.tsx
@@ -1,15 +1,12 @@
 declare namespace SheetTypes {
   type Clef = "treble" | "bass" | "both";
 
-  type Duration = "15" | "30" | "60";
-
   type TimeSignature = "4/4"; //more to be added later on
 
   interface SheetSettings {
     showFlats: boolean;
     showSharps: boolean;
     clef: Clef;
-    duration: Duration;
     timeSignature: TimeSignature;
     userStart?: boolean;
   }
