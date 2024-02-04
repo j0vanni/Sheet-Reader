ad490e7694fd686f53c15fae00872772f35ec8c1
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-04 15:59:18 -0500

    - Updated package.json and package-lock.json to include abcjs dependency. - Created Notation component to leverage abcjs for rendering music notations. - Added NotationTypes definitions for type safety in Notation component. - Adapted main.tsx and SheetMusic.tsx to utilize the new Notation component for ABC notation rendering.

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import Piano from "./ts/piano/Piano.tsx";
+import SheetMusic from "./ts/SheetMusic/SheetMusic.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Piano />
+    <SheetMusic />
   </React.StrictMode>
 );
--- front-end/src/ts/SheetMusic/SheetMusic.tsx
+++ front-end/src/ts/SheetMusic/SheetMusic.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Notation from "../Notation/Notation";
 
 const SheetMusic: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      hello!
+      <Notation notation="CDEFC|Gcd" />
+    </div>
+  );
 };
 
 export default SheetMusic;
