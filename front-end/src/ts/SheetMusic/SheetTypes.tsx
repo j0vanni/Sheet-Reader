759853de016a236a2a3309977fb0a8db2ffb2f9c
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-06 20:17:10 +0000

    Added Scale enum and updated SheetSettings interface

--- front-end/src/ts/SheetMusic/SheetTypes.tsx
+++ front-end/src/ts/SheetMusic/SheetTypes.tsx
@@ -1,13 +1,41 @@
 declare namespace SheetTypes {
-  type Clef = "treble" | "bass" | "both";
+  enum Scale {
+    CMajor = 'C\n',
+    GMajor = 'G\n',
+    DMajor = 'D\n',
+    AMajor = 'A\n',
+    EMajor = 'E\n',
+    BMajor = 'B\n',
+    FSharpMajor = 'F#\n',
+    CSharpMajor = 'C#\n',
+    FMajor = 'F\n',
+    BFlatMajor = 'Bb\n',
+    EFlatMajor = 'Eb\n',
+    AFlatMajor = 'Ab\n',
+    AMinor = 'Am\n',
+    EMinor = 'Em\n',
+    BMinor = 'Bm\n',
+    FSharpMinor = 'F#m\n',
+    CSharpMinor = 'C#m\n',
+    GSharpMinor = 'G#m\n',
+    DSharpMinor = 'D#m\n',
+    ASharpMinor = 'A#m\n',
+    DMinor = 'Dm\n',
+    GMinor = 'Gm\n',
+    CMinor = 'Cm\n',
+    FMinor = 'Fm\n',
+  }
+
+  type Clef = "treble" | "bass";
 
   type TimeSignature = "4/4"; //more to be added later on
 
   interface SheetSettings {
-    showFlats: boolean;
-    showSharps: boolean;
     clef: Clef;
+    scale: Scale;
     timeSignature: TimeSignature;
-    userStart?: boolean;
+    notation: string;
   }
 }
+
+export default SheetTypes;
