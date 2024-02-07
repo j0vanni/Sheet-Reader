9ec7f1a78db668ab3d0e397cce7b87c1af1cddc2
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-07 18:38:01 -0500

    Changed component from SheetMusic to MainPage

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -4,9 +4,10 @@ import App from "./App.tsx";
 import Piano from "./ts/piano/Piano.tsx";
 import SheetMusic from "./ts/SheetMusic/SheetMusic.tsx";
 import "./index.css";
+import MainPage from "./ts/MainPage/MainPage.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SheetMusic />
+    <MainPage />
   </React.StrictMode>
 );
