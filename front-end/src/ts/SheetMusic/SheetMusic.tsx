4e6637551104d9269b73e4d9d4c901004d924e1a
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-07 18:37:00 -0500

    Fix import and add renderOptions to SheetMusic component

--- front-end/src/ts/SheetMusic/SheetMusic.tsx
+++ front-end/src/ts/SheetMusic/SheetMusic.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Notation from "../Notation/Notation";
-import SheetTypes from './SheetTypes'
+import SheetTypes from "./SheetTypes";
 
 interface SheetMusicProps {
   settings: SheetTypes.SheetSettings;
 }
 
-const SheetMusic: React.FC<SheetMusicProps> = ({settings }) => {
-  let notation = 'K:${settings.scale} clef=${settings.clef}\n${settings.notation}'
+const SheetMusic: React.FC<SheetMusicProps> = ({ settings }) => {
+  let notation = `K:${settings.scale} clef=${settings.clef}\n${settings.notation}`;
+
   return (
     <div>
-      <Notation notation={}/>
+      <Notation notation={notation} renderOptions={settings.renderOptions} />
     </div>
   );
 };
--- front-end/src/ts/SheetMusic/SheetTypes.tsx
+++ front-end/src/ts/SheetMusic/SheetTypes.tsx
@@ -1,40 +1,45 @@
+import { RenderOptions } from "../Notation/NotationTypes";
+
 declare namespace SheetTypes {
   enum Scale {
-    CMajor = 'C\n',
-    GMajor = 'G\n',
-    DMajor = 'D\n',
-    AMajor = 'A\n',
-    EMajor = 'E\n',
-    BMajor = 'B\n',
-    FSharpMajor = 'F#\n',
-    CSharpMajor = 'C#\n',
-    FMajor = 'F\n',
-    BFlatMajor = 'Bb\n',
-    EFlatMajor = 'Eb\n',
-    AFlatMajor = 'Ab\n',
-    AMinor = 'Am\n',
-    EMinor = 'Em\n',
-    BMinor = 'Bm\n',
-    FSharpMinor = 'F#m\n',
-    CSharpMinor = 'C#m\n',
-    GSharpMinor = 'G#m\n',
-    DSharpMinor = 'D#m\n',
-    ASharpMinor = 'A#m\n',
-    DMinor = 'Dm\n',
-    GMinor = 'Gm\n',
-    CMinor = 'Cm\n',
-    FMinor = 'Fm\n',
+    CMajor = "C\n",
+    GMajor = "G\n",
+    DMajor = "D\n",
+    AMajor = "A\n",
+    EMajor = "E\n",
+    BMajor = "B\n",
+    FSharpMajor = "F#\n",
+    CSharpMajor = "C#\n",
+    FMajor = "F\n",
+    BFlatMajor = "Bb\n",
+    EFlatMajor = "Eb\n",
+    AFlatMajor = "Ab\n",
+    AMinor = "Am\n",
+    EMinor = "Em\n",
+    BMinor = "Bm\n",
+    FSharpMinor = "F#m\n",
+    CSharpMinor = "C#m\n",
+    GSharpMinor = "G#m\n",
+    DSharpMinor = "D#m\n",
+    ASharpMinor = "A#m\n",
+    DMinor = "Dm\n",
+    GMinor = "Gm\n",
+    CMinor = "Cm\n",
+    FMinor = "Fm\n",
   }
 
   type Clef = "treble" | "bass";
 
-  type TimeSignature = "4/4"; //more to be added later on
+  type TimeSignature = "4/4"; //more to be added later
+
+  type ScaleType = keyof typeof Scale;
 
   interface SheetSettings {
     clef: Clef;
-    scale: Scale;
+    scale: ScaleType;
     timeSignature: TimeSignature;
     notation: string;
+    renderOptions?: RenderOptions;
   }
 }
 
