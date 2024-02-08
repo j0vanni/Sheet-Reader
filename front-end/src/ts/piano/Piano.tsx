fa158f15a9f7fe70d8b3ab740eb2652d4e1b2574
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-08 15:41:30 -0500

    -added reset button -added 'animation' to reset button -added tabindex to reset button

--- front-end/src/ts/piano/Piano.tsx
+++ front-end/src/ts/piano/Piano.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Piano.css";
 import { PianoKey, Note } from "./PianoTypes";
 
@@ -17,14 +17,22 @@ const keys: PianoKey[] = [
   { note: Note.B4, whiteKey: true },
 ];
 
-
 const Piano: React.FC = () => {
+  const [isRotated, setIsRotated] = useState(false);
+  const [rotDeg, setRotDeg] = useState(0);
+
+  const handleReset = () => {
+    setIsRotated(true);
+    setTimeout(() => setIsRotated(false), 1000);
+  };
+
   const whiteKeys = keys.map((key, index) =>
     key.whiteKey ? (
       <button
         key={index}
         className="white-key"
         onClick={() => console.log(key.note)}
+        tabIndex={-1}
       ></button>
     ) : null
   );
@@ -35,14 +43,50 @@ const Piano: React.FC = () => {
         key={index}
         className="black-key"
         onClick={() => console.log(key.note)}
+        tabIndex={-1}
       ></button>
     ) : null
   );
 
   return (
-    <div className="piano-container">
-      <div className="white-keys-container">{whiteKeys}</div>
-      <div className="black-keys-container">{blackKeys}</div>
+    <div>
+      <div className="reset-button">
+        <button
+          onClick={handleReset}
+          style={
+            isRotated
+              ? {
+                  transform: `rotate(${rotDeg + 360}deg)`,
+                  transition: "all 1s ease",
+                }
+              : undefined
+          }
+          tabIndex={1}
+        >
+          <svg
+            width="17"
+            height="20"
+            viewBox="0 0 17 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15.9787 10.735C15.9894 10.9052 16 11.0754 16 11.2562C16 15.3945 12.6383 18.7562 8.5 18.7562C4.3617 18.7456 1 15.3945 1 11.2456C1 7.10731 4.3617 3.74561 8.5 3.74561C9.40426 3.74561 10.266 3.90518 11.0638 4.19241"
+              stroke="white"
+              stroke-width="1.68"
+            />
+            <path
+              d="M8.34029 1L11.4148 4.07447L8.22327 7.2766"
+              stroke="white"
+              stroke-width="0.98"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="piano-container">
+        <div className="white-keys-container">{whiteKeys}</div>
+        <div className="black-keys-container">{blackKeys}</div>
+      </div>
     </div>
   );
 };
