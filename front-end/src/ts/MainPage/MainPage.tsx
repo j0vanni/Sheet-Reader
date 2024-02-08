4ef44f646f95e0951790c7d7af3461865c4c1194
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-08 13:42:22 -0500

    Create MainPage - Added SheetMusic - Added Piano

--- front-end/src/ts/MainPage/MainPage.tsx
+++ front-end/src/ts/MainPage/MainPage.tsx
@@ -6,34 +6,39 @@ import Piano from "../Piano/Piano";
 const MainPage: React.FC = () => {
   return (
     <div className="container">
-      <SheetMusic
-        settings={{
-          timeSignature: "4/4",
-          scale: "CMajor",
-          clef: "treble",
-          notation: "C2 D2 E2 F2 G2 A2 B2 C2 D2 E2 F2 G2 A2 B2",
-          renderOptions: {
-            scale: 2,
-            paddingbottom: 1,
-            paddingright: 1,
-            paddingleft: 1,
-          },
-        }}
-      />
-      <SheetMusic
-        settings={{
-          timeSignature: "4/4",
-          scale: "CMajor",
-          clef: "bass",
-          notation: "CDEFGAB",
-          renderOptions: {
-            scale: 2,
-            paddingtop: 1,
-            paddingright: 1,
-            paddingleft: 1,
-          },
-        }}
-      />
+      <div className="sm-container">
+        <SheetMusic
+          settings={{
+            timeSignature: "4/4",
+            scale: "CMajor",
+            clef: "treble",
+            notation: ["C2", "D2", "E2", "F2", "G2", "A2", "B2", "C'2"],
+            renderOptions: {
+              scale: 1.5,
+              paddingbottom: 1,
+              paddingright: 1,
+              paddingleft: 1,
+              expandToWidest: true,
+              staffwidth: 600,
+            },
+          }}
+        />
+        <SheetMusic
+          settings={{
+            timeSignature: "4/4",
+            scale: "CMajor",
+            clef: "bass",
+            notation: ["C,2", "D,2", "E,2", "F,2", "G,2", "A,2", "B,2", "C,2"],
+            renderOptions: {
+              scale: 1.5,
+              paddingtop: 1,
+              paddingright: 1,
+              paddingleft: 1,
+              staffwidth: 600,
+            },
+          }}
+        />
+      </div>
       <div className="piano-container">
         <Piano />
       </div>
