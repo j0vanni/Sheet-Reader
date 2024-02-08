67f49c374b00950824099f199c90e12d832c3143
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-08 14:53:54 -0500

    - Created buttons - Added options to the buttons - Added glow to selected buttons

--- front-end/src/ts/Options/Options.tsx
+++ front-end/src/ts/Options/Options.tsx
@@ -2,9 +2,41 @@ import React from "react";
 import "./Options.css";
 
 const Options: React.FC = () => {
+  const [treble, setTreble] = React.useState(true);
+  const [bass, setBass] = React.useState(true);
+  const [sharps, setSharps] = React.useState(false);
+  const [flats, setFlats] = React.useState(false);
+  const [seconds, setSeconds] = React.useState(30);
+  const [tempo, setTempo] = React.useState(false);
+
   return (
-    <div className="container">
-      <p></p>
+    <div className="op-container">
+      <button onClick={() => setTreble(!treble)}>
+        <p className={treble ? "selected" : "unselected"}>Treble</p>
+      </button>
+      <button onClick={() => setBass(!bass)}>
+        <p className={bass ? "selected" : "unselected"}>Bass</p>
+      </button>
+      <button onClick={() => setSharps(!sharps)}>
+        <p className={sharps ? "selected" : "unselected"}>Sharps</p>
+      </button>
+      <button onClick={() => setFlats(!flats)}>
+        <p className={flats ? "selected" : "unselected"}>Flats</p>
+      </button>
+      <p id="divide"></p>
+      <button onClick={() => setSeconds(15)}>
+        <p className={seconds === 15 ? "selected" : "unselected"}>15 seconds</p>
+      </button>
+      <button onClick={() => setSeconds(30)}>
+        <p className={seconds === 30 ? "selected" : "unselected"}>30 seconds</p>
+      </button>
+      <button onClick={() => setSeconds(60)}>
+        <p className={seconds === 60 ? "selected" : "unselected"}>60 seconds</p>
+      </button>
+      <p id="divide"></p>
+      <button onClick={() => setTempo(!tempo)}>
+        <p className={tempo ? "selected" : "unselected"}>Match Tempo</p>
+      </button>
     </div>
   );
 };
