27dd4b3ac5cb00d3a97045acd0c4c7f015f26152
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-08 13:46:49 -0500

    Add Options component import

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -5,6 +5,7 @@ import Piano from "./ts/piano/Piano.tsx";
 import SheetMusic from "./ts/SheetMusic/SheetMusic.tsx";
 import "./index.css";
 import MainPage from "./ts/MainPage/MainPage.tsx";
+import Options from "./ts/Options/Options.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
