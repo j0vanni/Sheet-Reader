4789338c92c7fe514fa06012b42779bb0c85903f
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-08 13:45:00 -0500

    Create Options for bar above the sheet music notation to allow for customization, etc.

---
+++ front-end/src/ts/Options/Options.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Options.css";
+
+const Options: React.FC = () => {
+  return (
+    <div className="container">
+      <p></p>
+    </div>
+  );
+};
+
+export default Options;
