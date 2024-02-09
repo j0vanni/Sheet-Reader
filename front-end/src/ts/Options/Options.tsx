094b88b698fd7bced277ebead180ca45778fad88
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-08 22:33:37 -0500

    Changed dimensions for container, buttons, and font size

--- front-end/src/ts/Options/Options.tsx
+++ front-end/src/ts/Options/Options.tsx
@@ -11,32 +11,44 @@ const Options: React.FC = () => {
 
   return (
     <div className="op-container" tabIndex={-1}>
-      <button onClick={() => setTreble(!treble)} tabIndex={-1}>
-        <p className={treble ? "selected" : "unselected"}>Treble</p>
-      </button>
-      <button onClick={() => setBass(!bass)} tabIndex={-1}>
-        <p className={bass ? "selected" : "unselected"}>Bass</p>
-      </button>
-      <button onClick={() => setSharps(!sharps)} tabIndex={-1}>
-        <p className={sharps ? "selected" : "unselected"}>Sharps</p>
-      </button>
-      <button onClick={() => setFlats(!flats)} tabIndex={-1}>
-        <p className={flats ? "selected" : "unselected"}>Flats</p>
-      </button>
+      <div>
+        <button onClick={() => setTreble(!treble)} tabIndex={-1}>
+          <p className={treble ? "selected" : "unselected"}>Treble</p>
+        </button>
+        <button onClick={() => setBass(!bass)} tabIndex={-1}>
+          <p className={bass ? "selected" : "unselected"}>Bass</p>
+        </button>
+        <button onClick={() => setSharps(!sharps)} tabIndex={-1}>
+          <p className={sharps ? "selected" : "unselected"}>Sharps</p>
+        </button>
+        <button onClick={() => setFlats(!flats)} tabIndex={-1}>
+          <p className={flats ? "selected" : "unselected"}>Flats</p>
+        </button>
+      </div>
       <p id="divide"></p>
-      <button onClick={() => setSeconds(15)} tabIndex={-1}>
-        <p className={seconds === 15 ? "selected" : "unselected"}>15 seconds</p>
-      </button>
-      <button onClick={() => setSeconds(30)} tabIndex={-1}>
-        <p className={seconds === 30 ? "selected" : "unselected"}>30 seconds</p>
-      </button>
-      <button onClick={() => setSeconds(60)} tabIndex={-1}>
-        <p className={seconds === 60 ? "selected" : "unselected"}>60 seconds</p>
-      </button>
+      <div>
+        <button onClick={() => setSeconds(15)} tabIndex={-1}>
+          <p className={seconds === 15 ? "selected" : "unselected"}>
+            15 seconds
+          </p>
+        </button>
+        <button onClick={() => setSeconds(30)} tabIndex={-1}>
+          <p className={seconds === 30 ? "selected" : "unselected"}>
+            30 seconds
+          </p>
+        </button>
+        <button onClick={() => setSeconds(60)} tabIndex={-1}>
+          <p className={seconds === 60 ? "selected" : "unselected"}>
+            60 seconds
+          </p>
+        </button>
+      </div>
       <p id="divide"></p>
-      <button onClick={() => setTempo(!tempo)} tabIndex={-1}>
-        <p className={tempo ? "selected" : "unselected"}>Match Tempo</p>
-      </button>
+      <div>
+        <button onClick={() => setTempo(!tempo)} tabIndex={-1}>
+          <p className={tempo ? "selected" : "unselected"}>Match Tempo</p>
+        </button>
+      </div>
     </div>
   );
 };
