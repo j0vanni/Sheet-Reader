f810c04c69d8e897cd225109ba28a847d9878060
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-10 15:48:52 -0500

    - add sheetmusic generation - sharps and flats selection works now - added sharp/flat percentage, line break length

--- front-end/src/ts/MainPage/MainPage.tsx
+++ front-end/src/ts/MainPage/MainPage.tsx
@@ -1,11 +1,119 @@
 import React, { useEffect, useState } from "react";
 import "./MainPage.css";
 import SheetMusic from "../SheetMusic/SheetMusic";
-import Piano from "../Piano/Piano";
+import Piano from "../piano/Piano";
 import Options from "../Options/Options";
+import { Note } from "../piano/PianoTypes";
+import { bassNoteUsage, trebleNoteUsage } from "./NoteUsage";
 
 const MainPage: React.FC = () => {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [treble, setTreble] = useState(true);
+  const [bass, setBass] = useState(true);
+  const [sharps, setSharps] = useState(false);
+  const [flats, setFlats] = useState(false);
+  const [seconds, setSeconds] = useState(30);
+  const [tempo, setTempo] = useState(false);
+  const flatsharpPercentage = 0.2;
+  const noteGenerationLength = 12;
+  const lineBreakLength = 4;
+
+  const [trebleNotation, setTrebleNotation] = useState(["c,2"]);
+  const [bassNotation, setBassNotation] = useState(["c,,,2"]);
+
+  const handlePianoKeyPress = (note: Note) => {
+    console.log(note);
+  };
+
+  const handleResetPress = () => {
+    generateTrebleNotation(noteGenerationLength, sharps, flats);
+    generateBassNotation(noteGenerationLength, sharps, flats);
+  };
+
+  const noteToNotation = (note: string, sf: string) => {
+    let newNote = "";
+    if (sf !== "") {
+      newNote = sf + note.substring(0, 1);
+    } else {
+      newNote = note.substring(0, 1);
+    }
+    const octave = Number(note.substring(1, 2));
+
+    if (octave < 4) {
+      newNote += ",".repeat(4 - octave);
+    } else if (octave > 4) {
+      newNote += "'".repeat(octave - 4);
+    }
+    newNote += "2";
+    return newNote;
+  };
+
+  const sharpsOrFlats = (sharps: boolean, flats: boolean) => {
+    let sf = "";
+    if (sharps) {
+      if (Math.random() < flatsharpPercentage) {
+        sf += "^";
+      }
+    }
+    if (flats && sf === "") {
+      if (Math.random() < flatsharpPercentage) {
+        sf += "_";
+      }
+    }
+
+    return sf;
+  };
+
+  const selectWeightedNote = (noteUsageArray: Array<any>) => {
+    const totalWeight = noteUsageArray.reduce(
+      (acc, { percentage }) => acc + percentage,
+      0
+    );
+    const randomNum = Math.random() * totalWeight;
+
+    let weightSum = 0;
+    for (const noteUsage of noteUsageArray) {
+      weightSum += noteUsage.percentage;
+      if (randomNum <= weightSum) {
+        return noteUsage;
+      }
+    }
+    return noteUsageArray[noteUsageArray.length - 1];
+  };
+
+  const generateTrebleNotation = (
+    length: number,
+    sharps: boolean,
+    flats: boolean
+  ) => {
+    let notation = [];
+    for (let i = 0; i < length; i++) {
+      if (i % lineBreakLength === 0) {
+        notation.push("|");
+      }
+      const randomNote = selectWeightedNote(trebleNoteUsage).note;
+      notation.push(noteToNotation(randomNote, sharpsOrFlats(sharps, flats)));
+    }
+    setTrebleNotation(notation);
+  };
+
+  const generateBassNotation = (
+    length: number,
+    sharps: boolean,
+    flats: boolean
+  ) => {
+    let notation = [];
+    for (let i = 0; i < length; i++) {
+      if (i % lineBreakLength === 0) {
+        notation.push("|");
+      }
+      const randomNote = selectWeightedNote(bassNoteUsage).note;
+
+      notation.push(noteToNotation(randomNote, sharpsOrFlats(sharps, flats)));
+    }
+    console.log(notation);
+    setBassNotation(notation);
+  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -17,12 +125,25 @@ const MainPage: React.FC = () => {
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [trebleNotation, bassNotation]);
 
   return (
     <div className="container">
       <div className="options-container">
-        <Options />
+        <Options
+          treble={treble}
+          setTreble={setTreble}
+          bass={bass}
+          setBass={setBass}
+          sharps={sharps}
+          setSharps={setSharps}
+          flats={flats}
+          setFlats={setFlats}
+          seconds={seconds}
+          setSeconds={setSeconds}
+          tempo={tempo}
+          setTempo={setTempo}
+        />
       </div>
       <div className="sm-container">
         <SheetMusic
@@ -30,7 +151,7 @@ const MainPage: React.FC = () => {
             timeSignature: "4/4",
             scale: "CMajor",
             clef: "treble",
-            notation: ["C2", "D2", "E2", "F2", "G2", "A2", "B2", "C'2"],
+            notation: trebleNotation,
             renderOptions: {
               scale: 1.5,
               paddingbottom: 1,
@@ -45,7 +166,7 @@ const MainPage: React.FC = () => {
             timeSignature: "4/4",
             scale: "CMajor",
             clef: "bass",
-            notation: ["C,2", "D,2", "E,2", "F,2", "G,2", "A,2", "B,2", "C,2"],
+            notation: bassNotation,
             renderOptions: {
               scale: 1.5,
               paddingtop: 1,
@@ -57,7 +178,10 @@ const MainPage: React.FC = () => {
         />
       </div>
       <div className="piano-container" tabIndex={1}>
-        <Piano />
+        <Piano
+          onKeyPress={handlePianoKeyPress}
+          onResetPress={handleResetPress}
+        />
       </div>
     </div>
   );
