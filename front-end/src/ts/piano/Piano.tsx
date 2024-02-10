16815e265ca688ea413902d6c3fa5c2ec95283d6
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-10 15:50:23 -0500

    - update reset button handling - fixed stroke-width to strokeWidth - updated notes to new var naming

--- front-end/src/ts/piano/Piano.tsx
+++ front-end/src/ts/piano/Piano.tsx
@@ -1,28 +1,33 @@
 import React, { useState } from "react";
 import "./Piano.css";
-import { PianoKey, Note } from "./PianoTypes";
+import { PianoKey, Note, PianoProps } from "./PianoTypes";
 
 const keys: PianoKey[] = [
-  { note: Note.C4, whiteKey: true },
-  { note: Note.CD4, blackKey: true },
-  { note: Note.D4, whiteKey: true },
-  { note: Note.DE4, blackKey: true },
-  { note: Note.E4, whiteKey: true },
-  { note: Note.F4, whiteKey: true },
-  { note: Note.FG4, blackKey: true },
-  { note: Note.G4, whiteKey: true },
-  { note: Note.GA4, blackKey: true },
-  { note: Note.A4, whiteKey: true },
-  { note: Note.AB4, blackKey: true },
-  { note: Note.B4, whiteKey: true },
+  { note: Note.C5, whiteKey: true },
+  { note: Note.CD5, blackKey: true },
+  { note: Note.D5, whiteKey: true },
+  { note: Note.DE5, blackKey: true },
+  { note: Note.E5, whiteKey: true },
+  { note: Note.F5, whiteKey: true },
+  { note: Note.FG5, blackKey: true },
+  { note: Note.G5, whiteKey: true },
+  { note: Note.GA5, blackKey: true },
+  { note: Note.A5, whiteKey: true },
+  { note: Note.AB5, blackKey: true },
+  { note: Note.B5, whiteKey: true },
 ];
 
-const Piano: React.FC = () => {
+const Piano: React.FC<PianoProps> = ({ onKeyPress, onResetPress }) => {
   const [isRotated, setIsRotated] = useState(false);
 
   const handleReset = () => {
     setIsRotated(true);
     setTimeout(() => setIsRotated(false), 1000);
+    onResetPress();
+  };
+
+  const handleKeyPress = (note: Note) => {
+    onKeyPress(note);
   };
 
   const whiteKeys = keys.map((key, index) =>
@@ -30,7 +35,7 @@ const Piano: React.FC = () => {
       <button
         key={index}
         className="white-key"
-        onClick={() => console.log(key.note)}
+        onClick={() => handleKeyPress(key.note)}
         tabIndex={-1}
       ></button>
     ) : null
@@ -41,7 +46,7 @@ const Piano: React.FC = () => {
       <button
         key={index}
         className="black-key"
-        onClick={() => console.log(key.note)}
+        onClick={() => handleKeyPress(key.note)}
         tabIndex={-1}
       ></button>
     ) : null
@@ -72,12 +77,12 @@ const Piano: React.FC = () => {
             <path
               d="M15.9787 10.735C15.9894 10.9052 16 11.0754 16 11.2562C16 15.3945 12.6383 18.7562 8.5 18.7562C4.3617 18.7456 1 15.3945 1 11.2456C1 7.10731 4.3617 3.74561 8.5 3.74561C9.40426 3.74561 10.266 3.90518 11.0638 4.19241"
               stroke="white"
-              stroke-width="1.68"
+              strokeWidth="1.68"
             />
             <path
               d="M8.34029 1L11.4148 4.07447L8.22327 7.2766"
               stroke="white"
-              stroke-width="0.98"
+              strokeWidth="0.98"
             />
           </svg>
         </button>
