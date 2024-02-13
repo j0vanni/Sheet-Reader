6ddbf4484d98c2f817f6c7c9d1e216b37d73162f
j0vanni <49281020+j0vanni@users.noreply.github.com>
2024-02-12 19:11:14 -0500

    moved note generation from mainpage to its own file, moved that to the utils folder

---
+++ front-end/src/ts/Utils/noteGenerationUtils.tsx
@@ -0,0 +1,174 @@
+import { trebleNoteUsage, bassNoteUsage } from "./NoteUsage";
+
+export const generateNotes = (
+  length: number,
+  sharps: boolean,
+  flats: boolean,
+  linebreak: number,
+  twoHands: boolean,
+  percentage: number
+) => {
+  if (twoHands) {
+    const trebleNotation = generateTrebleNotation(
+      length,
+      sharps,
+      flats,
+      linebreak,
+      percentage
+    );
+    const bassNotation = generateBassNotation(
+      length,
+      sharps,
+      flats,
+      linebreak,
+      percentage
+    );
+
+    return [trebleNotation, bassNotation];
+  } else {
+    const notation = twoHandedNotes(
+      length,
+      sharps,
+      flats,
+      linebreak,
+      percentage
+    );
+    if (notation) {
+      //notation[0] = treble, notation[1] = bass
+      return [notation[0], notation[1]];
+    }
+  }
+};
+
+export const generateTrebleNotation = (
+  length: number,
+  sharps: boolean,
+  flats: boolean,
+  linebreak: number,
+  percentage: number
+) => {
+  let notation = [];
+  for (let i = 0; i < length; i++) {
+    if (i % linebreak === 0) {
+      notation.push("|");
+    }
+    const randomNote = selectWeightedNote(trebleNoteUsage).note;
+    notation.push(
+      noteToNotation(randomNote, sharpsOrFlats(sharps, flats, percentage))
+    );
+  }
+  return notation;
+};
+
+export const generateBassNotation = (
+  length: number,
+  sharps: boolean,
+  flats: boolean,
+  linebreak: number,
+  percentage: number
+) => {
+  let notation: string[] = [];
+  for (let i = 0; i < length; i++) {
+    if (i % linebreak === 0) {
+      notation.push("|");
+    }
+    const randomNote = selectWeightedNote(bassNoteUsage).note;
+
+    notation.push(
+      noteToNotation(randomNote, sharpsOrFlats(sharps, flats, percentage))
+    );
+  }
+  return notation;
+};
+
+const twoHandedNotes = (
+  length: number,
+  sharps: boolean,
+  flats: boolean,
+  linebreak: number,
+  percentage: number
+) => {
+  let arr = [];
+  for (let i = 0; i < length; i++) {
+    if (Math.random() < 0.5) {
+      arr.push("treble");
+    } else {
+      arr.push("bass");
+    }
+  }
+  let trebleNotation = [];
+  let bassNotation = [];
+
+  for (let i = 0; i < length; i++) {
+    if (i % linebreak === 0) {
+      trebleNotation.push("|");
+      bassNotation.push("|");
+    }
+    if (arr[i] === "treble") {
+      const randomNote = selectWeightedNote(trebleNoteUsage).note;
+      trebleNotation.push(
+        noteToNotation(randomNote, sharpsOrFlats(sharps, flats, percentage))
+      );
+      bassNotation.push("x2");
+    } else {
+      const randomNote = selectWeightedNote(bassNoteUsage).note;
+      bassNotation.push(
+        noteToNotation(randomNote, sharpsOrFlats(sharps, flats, percentage))
+      );
+      trebleNotation.push("x2");
+    }
+  }
+
+  return [trebleNotation, bassNotation];
+};
+
+const noteToNotation = (note: string, sf: string) => {
+  let newNote = "";
+  if (sf !== "") {
+    newNote = sf + note.substring(0, 1);
+  } else {
+    newNote = note.substring(0, 1);
+  }
+  const octave = Number(note.substring(1, 2));
+
+  if (octave < 4) {
+    newNote += ",".repeat(4 - octave);
+  } else if (octave > 4) {
+    newNote += "'".repeat(octave - 4);
+  }
+  newNote += "2";
+  return newNote;
+};
+
+const sharpsOrFlats = (sharps: boolean, flats: boolean, percentage: number) => {
+  let sf = "";
+  if (sharps) {
+    if (Math.random() < percentage) {
+      sf += "^";
+    }
+  }
+  if (flats && sf === "") {
+    if (Math.random() < percentage) {
+      sf += "_";
+    }
+  }
+
+  return sf;
+};
+
+const selectWeightedNote = (noteUsageArray: Array<any>) => {
+  const totalWeight = noteUsageArray.reduce(
+    (acc, { percentage }) => acc + percentage,
+    0
+  );
+  const randomNum = Math.random() * totalWeight;
+
+  let weightSum = 0;
+  for (const noteUsage of noteUsageArray) {
+    weightSum += noteUsage.percentage;
+    if (randomNum <= weightSum) {
+      return noteUsage;
+    }
+  }
+  return noteUsageArray[noteUsageArray.length - 1];
+};
